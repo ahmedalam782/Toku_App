238af80879cd1fabfbac41cd0bb3b9d8b6e201f1
ahmedmohamed <ahmedalam4887482@gmail.com>
2024-05-04 03:28:47 +0300

    create app that show number and Family Member and Colors and Phrases and hear it when pronounce and audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         PhrasesScreen.routeName: (_) => const PhrasesScreen(),
       },
       initialRoute: HomeScreen.routeName,
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
     );
   }
 }
